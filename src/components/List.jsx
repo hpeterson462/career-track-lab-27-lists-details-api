64f6c88f66eb117dd0f2da7e2bc72da2d9663a58
Heather Peterson <hpeterson462@gmail.com>
2020-10-20 21:27:09 -0700

    list

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -8,8 +8,13 @@ export default class ApiData extends Component {
 
   handleButtonClick = async () => {
     const characters = await getApi();
-    console.log(characters);
+
     this.setState({ characters });
+  }
+
+  handleNameClick = async (e) => {
+    e.preventDefault();
+    this.props.history.push(`/Details/${character._id}`)
   }
 
 
@@ -20,7 +25,7 @@ export default class ApiData extends Component {
         <ul>
           {this.state.characters.map(character => (
             <li key={character._id}>
-              <a>{character.name}</a>
+              <a onClick={this.handleNameClick}>{character.name}</a>
             </li>))}
         </ul>
         <button onClick={this.handleButtonClick}>Get Characters</button>
